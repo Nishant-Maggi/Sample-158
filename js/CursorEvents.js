09772c433955eec89afdf7cb36e49105f1557716
Nishant-Maggi <62693414+Nishant-Maggi@users.noreply.github.com>
2021-11-15 12:49:01 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,35 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: {default: "",type: "string"},
+  },
+  init: function(){
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+  },
+  handlePlacesListState: function(){
+    const id = this.el.getAttribute("id");
+    const placesId = ["avengers", "wonder-woman", "flash", "captain-marvel"]
+    if(placesId.includes(id)){
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {selectedItemId: id});
+      this.el.setAttribute("material", {color: "#D76B30", opacity: 1});
+    }
+  },
+  handleMouseEnterEvents: function(){
+    this.el.addEventListener("mouseenter", ()=>{
+      this.handlePlacesListState();
+    })
+  },
+  handleMouseLeaveEvents: function(){
+    this.el.addEventListener("mouseleave", ()=>{
+      const {selectedItemId} = this.data;
+      if(selectedItemId){
+        const el = document.querySelector(`#${selectedItemId}`);
+        const id = el.getAttribute("id");
+        if(id == selectedItemId){
+          el.setAttribute("material", {color: "#07C", opacity: 1});
+        }
+      }
+    });
+  }
+});
